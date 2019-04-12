703638c3fa07821945ee81f40c220d86482704a0
Michael Mingci Lu <mingci7@stanford.edu>
2019-04-12 13:44:36 -0700

    Added MemFold

--- labs/lab2.scala
+++ labs/lab2.scala
@@ -29,25 +29,35 @@ import spatial.dsl._
 
 
 // MemFold
-// @spatial object Lab2Part2SimpleMemFold extends SpatialApp {
-//
-//   val N = 16.to[Int]
-//
-//   def main(args: Array[String]): Unit = {
-//     val out = DRAM[Int](16)
-//     Accel {
-//       // Your code here
-//     }
-//
-//     val result = getMem(out)
-//     val gold = Array.tabulate(16){i => 10.to[Int]}
-//     printArray(gold, "expected: ")
-//     printArray(result, "result:   ")
-//
-//     val cksum = gold.zip(result){_==_}.reduce{_&&_}
-//     println("PASS: " + cksum + " (Lab2Part2SimpleMemFold)")
-//   }
-// }
+@spatial object Lab2Part2SimpleMemFold extends SpatialApp {
+
+  val N = 16.to[Int]
+
+  def main(args: Array[String]): Unit = {
+    val initial = args(0).to[Int]
+    val x = ArgIn[Int]
+    setArg(x, initial)
+    val out = DRAM[Int](16)
+    Accel {
+      val a = SRAM[Int](16)
+      Foreach(16 by 1) { k => a(k) = x}
+      MemFold(a)(-5 until 5 by 1){i =>
+        val tmp = SRAM[Int](16)
+        Foreach(16 by 1) { j => tmp(j) = 1}
+        tmp
+      }{_+_}
+      out store a
+    }
+
+    val result = getMem(out)
+    val gold = Array.tabulate(16){i => 10.to[Int] + initial}
+    printArray(gold, "expected: ")
+    printArray(result, "result:   ")
+
+    val cksum = gold.zip(result){_==_}.reduce{_&&_}
+    println("PASS: " + cksum + " (Lab2Part2SimpleMemFold)")
+  }
+}
 
 
 // FSM

cfa5e2c8f070227018606bfeb4e917b6753f7686
Ron Domingo <rdomingo@tucson.Stanford.EDU>
2019-04-26 13:46:50 -0700

    Wrote udp-send functionality

--- labs/lab4/udp/udp-send.c
+++ labs/lab4/udp/udp-send.c
@@ -18,23 +18,43 @@ int main(void)
 	char buf[BUFSIZE];	/* message buffer */
 	int recvlen;		/* # bytes in acknowledgement message */
 
-  /* TODO: create a UDP socket */
+  // Create UDP socket 
+  if ((fd=socket(AF_INET, SOCK_DGRAM, 0))==-1) return 0;
 
-  /* TODO: bind the socket to all local addresses and pick any port number */
+  // Bind the socket to all local addresses and pick any port number 
+  struct sockaddr_in myaddr;
+  memset((char*)&myaddr, 0, sizeof(myaddr));
+  myaddr.sin_family = AF_INET;
+  myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  myaddr.sin_port = htons(0);
 
-
+  if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) { 
+    perror("bind failed");
+    return 0;
+  }
 
   // Set message buffer
   char *message = "Thirsty Thursday.";
 
-  /* TODO: copy the message to the message buffer, buf */
+  // Copy the message to the message buffer, buf
+  memcpy(message, buf, sizeof(message));
 
-	/* TODO: now define remaddr, the address to whom we want to send messages. For convenience, the host address is expressed as a numeric IP address. We will convert this IP address to a binary format via inet_aton */
+	// Now define remaddr, the address to whom we want to send messages. For convenience, the host address is expressed as a numeric IP address. We will convert this IP address to a binary format via inet_aton 
+  memset((char *) &remaddr, 0, sizeof(remaddr));
+  remaddr.sin_family = AF_INET;
+  remaddr.sin_port = htons(SERVICE_PORT);
+  if (inet_aton(server, &remaddr.sin_addr)==0) {
+    fprintf(stderr, "inet_aton() failed\n");
+    exit(1);
+  }
 
-	/* TODO: now let's send the messages */
+	// Send the message
+  if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *)&remaddr, slen)==-1) {
+    perror("sendto");
+    exit(1);
+  } 
 
-    /* TODO: close fd */
-
+  // Close fd
 	close(fd);
 	return 0;
 }

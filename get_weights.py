5314e749c168ea0dfc35ca229772dabca2bfe72e
Ron Domingo <rondomingo@DN51utb0.SUNet>
2019-05-17 20:54:29 -0700

    The get_weights script can now parse all of the weights, biases, means, and variances from the layers. All that is left to do is restructure the data and save them as CSVs in the correct format.

--- get_weights.py
+++ get_weights.py
@@ -4,14 +4,27 @@ import numpy as np
 
 if __name__ == '__main__':
     
+    # Open the model
     f = open('colorization_release_v2.caffemodel', 'rb')
-    cq2 = cq.NetParameter()
-    cq2.ParseFromString(f.read())
+    model = cq.NetParameter()
+    model.ParseFromString(f.read())
     f.close()
-    for layer in cq2.layer:
-        print(layer.name)
-    layer = cq2.layer[11]
-    data = np.asarray(layer.blobs)
-    print(data[0].data)
-    #print(inspect.getmembers(layer))
 
+    # Loop through the layers of the CNN
+    for layer in model.layer:
+        print(layer.name)
+        # Get the weights from the convolution and norm layers only
+        if 'conv' in layer.name:
+            if 'norm' in layer.name:
+                # Norm layer format: (0) mean, (1) variance, (2) moving average factor
+                blobs = layer.blobs
+                for i in range(len(blobs[0].data)):
+                    print("{}\t{}".format(blobs[0].data[i], blobs[1].data[i]))
+            else:
+                # Conv layer format: (0) weights, (1) biases
+                blobs = layer.blobs
+                # Extract the weights and place them in their proper shape
+                weights = np.asarray(blobs[0].data).reshape(blobs[0].shape.dim)
+                print(weights.shape)
+                # Extract the biases for the layer
+                biases = np.asarray(blobs[1].data)

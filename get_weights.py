10dee2ae67a7413d2e95bb9e9ca7dde7ed562c6f
Ron Domingo <rondomingo@DN51utb0.SUNet>
2019-05-19 22:10:36 -0700

    The weights, means, variance, and the biases are now written to a file

--- get_weights.py
+++ get_weights.py
@@ -16,15 +16,27 @@ if __name__ == '__main__':
         # Get the weights from the convolution and norm layers only
         if 'conv' in layer.name:
             if 'norm' in layer.name:
+                filename = "{}_mean_variance.csv".format(layer.name)
                 # Norm layer format: (0) mean, (1) variance, (2) moving average factor
                 blobs = layer.blobs
-                for i in range(len(blobs[0].data)):
-                    print("{}\t{}".format(blobs[0].data[i], blobs[1].data[i]))
+                mean = np.asarray(blobs[0].data)
+                variance = np.asarray(blobs[1].data)
+                # Write the mean and variance to a file with the first column as mean
+                # and the second column as variance for each line
+                csv = np.column_stack((mean, variance))
+                np.savetxt(filename, csv, delimiter=',')
             else:
                 # Conv layer format: (0) weights, (1) biases
                 blobs = layer.blobs
                 # Extract the weights and place them in their proper shape
-                weights = np.asarray(blobs[0].data).reshape(blobs[0].shape.dim)
-                print(weights.shape)
+                weights_shape = blobs[0].shape.dim
+                # Reshape the weights to our desired indexing, reducing the shape of the 
+                # weights from 4 dims to 2 dims so that it can be placed in a csv file
+                weights = np.asarray(blobs[0].data).reshape(weights_shape)
+                weights = weights.reshape(weights_shape[0], weights_shape[1]*weights_shape[2]*weights_shape[3])
+                filename = "{}_weights.csv".format(layer.name)
+                np.savetxt(filename, weights, delimiter=',')
                 # Extract the biases for the layer
                 biases = np.asarray(blobs[1].data)
+                filename = "{}_bias.csv".format(layer.name)
+                np.savetxt(filename, biases, delimiter=',')

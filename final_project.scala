b2159eb536af3cd2122fb5f5974da255748de496
Michael Mingci Lu <mingci7@stanford.edu>
2019-05-19 17:25:58 -0700

    Updated scala file with first layer of conv written

--- final_project.scala
+++ final_project.scala
@@ -1,9 +1,7 @@
 @spatial object ProjectCNNColorization extends SpatialApp {
 
-  val Kh = 3
-  val Kw = 3
-  // Value of max column length = 512*3*3
-  val Cmax = 4608
+  val kernel_size = 3
+  val Cmax = 224
 
   // Can alter this for more precision
   type T = FixPt[TRUE, _5, _11]
@@ -14,87 +12,122 @@
     val midPar = 1
     val innerPar = 1
 
-    // val B = 256
+    // Importing weights and biases for all layers
+    val w1_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b1_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w1_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b1_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_1 = loadCSV1D[T]("XXXXX.csv", ",")
 
-    // val R = ArgIn[Int]
-    // val C = ArgIn[Int]
-    val R = 224
-    val C = 224
-    val border = 3
+    /*
+    val w2_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b2_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w2_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b2_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_2 = loadCSV1D[T]("XXXXX.csv", ",")
 
+    val w3_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b3_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w3_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b3_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w3_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b3_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_3 = loadCSV1D[T]("XXXXX.csv", ",")
 
-    val W_data = loadCSV2D[T]("XXXXX.csv", ",")
-    val b_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w4_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b4_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w4_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b4_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w4_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b4_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_4 = loadCSV1D[T]("XXXXX.csv", ",")
+
+    val w5_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b5_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w5_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b5_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w5_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b5_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_5 = loadCSV1D[T]("XXXXX.csv", ",")
+   
+    val w6_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b6_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w6_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b6_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w6_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b6_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_6 = loadCSV1D[T]("XXXXX.csv", ",")
+
+    val w7_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b7_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w7_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b7_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w7_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b7_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_7 = loadCSV1D[T]("XXXXX.csv", ",")
+    
+    val w8_1_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b8_1_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w8_2_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b8_2_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val w8_3_data = loadCSV2D[T]("XXXXX.csv", ",")
+    val b8_3_data = loadCSV1D[T]("XXXXX.csv", ",")
+    val norm_8 = loadCSV1D[T]("XXXXX.csv", ",")
+  */
+
     val test_image = loadCSV2D[T]("XXXXX.csv", ",")
     // ground truth image?? for comparision
     // val gold = loadCSV2D[T]("XXXXX.csv, ",")
     
-    val image_dram = DRAM[T](R, C)
-    val W = DRAM[T](64, 36)
-    val b = DRAM[T](64)
+    // Initialize weights and biases in DRAM
+    val image_dram = DRAM[T](Cmax, Cmax)
+    val w1_1 = DRAM[T](64, 9)
+    val b1_1 = DRAM[T](64)
     setMem(image_dram, test_image)
-    setMem(W_data, W)
-    setMem(b_data, b)
-    // setArg(R, image.rows)
-    // setArg(C, image.cols)
-    // val lb_par = 8
+    setMem(w1_1, w1_1_data)
+    setMem(b1_1, b1_1_data)
 
-    // val img = DRAM[T](R, C)
-    val output = DRAM[T](R, C)
+    // TODO: Initialize everything else...
+    val output = DRAM[T](Cmax, Cmax)
 
-    // setMem(img, image)
+    val conv1_1_result = conv1_1(image_dram)
+  } 
+
+  def conv1_1(image: Matrix[T]): Matrix[T] = {
+    val num_filters = 64
+    val lb_par = 8
+    val conv1_1_result = DRAM[T](Cmax, num_filters*Cmax)
 
     Accel {
-      val lb = LineBuffer[T](Kh, Cmax)
+      val lb = LineBuffer[T](kernel_size, Cmax)
 
-      val kh = LUT[T](6,6)(1.to[T], 0.to[T], -1.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           2.to[T], 0.to[T], -2.to[T],
-                           1.to[T], 0.to[T], -1.to[T])
-      val kv = LUT[T](6,6)(1.to[T],  2.to[T],  1.to[T],                                                                                0.to[T],  0.to[T],  0.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T],
-                           -1.to[T], -2.to[T], -1.to[T])
+      val filters = LUT[T](num_filters,9)
+      val bias = SRAM[T](num_filters)
+      filters load w1_1
+      bias load b1_1
+      
+      val sr = RegFile[T](kernel_size, kernel_size)
+      // 14336 = 64 * 224, all kernel results for one row
+      val lineOut = SRAM[T](num_filters*Cmax)
 
-      val sr = RegFile[T](Kh, Kw)
-      val lineOut = SRAM[T](Cmax)
-
-      Foreach (0 until R) {r =>
-        lb load img(r, 0::C par lb_par)
-        Foreach (0 until C) {c =>
+      Foreach (0 until Cmax) {r =>
+        lb load img(r, 0::Cmax par lb_par)
+        Foreach (0 until Cmax) {c =>
           Pipe{sr.reset(c==0)}
-          Foreach (0 until Kh) {i => sr(i,*) <<= lb(i,c)}
-          val horz = Reduce(Reg[T])(Kh by 1) { i =>
-            Reduce(0)(Kw by 1) { j => 
-              sr(i,j) * kh(i,j)
-            }{_+_}
-          }{_+_}
-          val vert = Reduce(Reg[T])(Kh by 1) { i =>
-            Reduce(0)(Kw by 1) { j => 
-              sr(i,j) * kv(i,j)
+          Foreach (0 until kernel_size) {i => sr(i,*) <<= lb(i,c)}
+          Foreach (0 until num_filters) { filter_index =>  
+            val result = MemFold(bias)(kernel_size by 1) { i =>
+              Reduce(0)(kernel_size by 1) { j => 
+                sr(i,j) * filters(filter_index,i*3 + j)
+              }{_+_}
             }{_+_} 
-          }{_+_}    
-          lineOut(c) = mux( r<5 || c<5, 0.to[T], abs(horz.value) + abs(vert.value)) 
+            lineOut(filter_index*Cmax + c) = mux( r<kernel_size || c<kernel_size, 0.to[T], max(0.to[T], result.value))
+          }
         }
-        imgOut(r, 0::C) store lineOut
+        conv1_1_result(r,0::num_filters*Cmax) store lineOut
       } 
     } 
-    
-    getMatrix(imgOut)
+    getMatrix(conv1_1_result)
   }
 }
+
